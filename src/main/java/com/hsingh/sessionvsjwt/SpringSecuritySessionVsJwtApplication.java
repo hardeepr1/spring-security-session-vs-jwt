1683d462eb0f74c6a760f62a46972bfd6567f4a6
Hardeep Raike <hardeeprai234@gmail.com>
2025-09-01 11:34:57 -0400

    Initialize project

---
+++ src/main/java/com/hsingh/sessionvsjwt/SpringSecuritySessionVsJwtApplication.java
@@ -0,0 +1,13 @@
+package com.hsingh.sessionvsjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySessionVsJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySessionVsJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsingh/sessionvsjwt/SpringSecuritySessionVsJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsingh.sessionvsjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySessionVsJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
